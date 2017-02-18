63ef7503dade993c16a59d226d7462ccf2941fb5
chenshunli <chenshunli@ulaiber.com>
2017-02-17 22:30:12 -0800

    列举Python中的高频的模式

---
+++ README.md
@@ -0,0 +1,1 @@
+# 参照[cl-python](https://common-lisp.net/project/clpython/),用Clojure将clj代码编译为python代码
---
+++ hello.py
@@ -0,0 +1,7 @@
+# #!/usr/bin/python
+#-*-coding:utf-8 -*-
+## 列举Python中的高频的模式,或者经常使用的不适应的模式
+# 1. lambda:
+lambda a: a
+# 2. for
+for i in 1,2: print i
